93a8f688af9b826528d07ff19f3658e514324a2b
jgooday48 <joelgooday48@gmail.com>
2023-11-03 10:51:38 +0000

    incorporate ansi-colors

--- app.js
+++ app.js
@@ -21,19 +21,19 @@ function game() {
         turns ++ // adds 1 to turns
 
         if (userNum === computerNum) { // ends game if num is correct
-            console.log('congrats! You have correctly guesses the number.')
+            console.log(c.green('congrats! You have correctly guesses the number.'))
             break
         }
 
         else if (turns < maxTries) { // allows another attempt
-            console.log(`Incorrect, try again. You have ${maxTries-turns} attempts left`)
+            console.log(c.red(`Incorrect, try again. You have ${maxTries-turns} attempts left`))
              
         }
         else if (turns === maxTries-1) { // allows one more attempt
-            console.log('Incorrect, you have 1 attempt left')
+            console.log(c.red('Incorrect, you have 1 attempt left'))
         }
         else { // if user failed to guess
-            
+           console.log(c.red('You have failed'))
            console.log(`Computers number was ${computerNum}`)
            break
         }
